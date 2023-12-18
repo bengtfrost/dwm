c27feb92fcec063a8079940c3eb1cb79692a7630
fhblf <fhblf@tutanota.com>
2023-12-18 10:50:55 +0100

    changed to monospace font

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 8;        /* 2 is the default spacing around the bar's font */
-static const char *fonts[]          = { "Fira Code:style=Regular:size=9" };
-static const char dmenufont[]       = "Fira Code:style=Regular:size=9";
+static const char *fonts[]          = { "monospace:style=Regular:size=9" };
+static const char dmenufont[]       = "monospace:style=Regular:size=9";
 // static const char col_gray1[]       = "#222222";
 // static const char col_gray2[]       = "#444444";
 // static const char col_gray3[]       = "#bbbbbb";
