f923b20453d577d0081986f03e40cf26ac53ad4a
fhblf <fhblf@tutanota.com>
2023-12-24 17:50:30 +0100

    changed colorscheme

--- config.h
+++ config.h
@@ -15,7 +15,8 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 8;        /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "monospace:style=Regular:size=9" };
 static const char dmenufont[]       = "monospace:style=Regular:size=9";
-static const char col_gray1[]       = "#222222";
+// static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
