de39f84a6d32b1fff1e1d1850db531117b8260df
fhblf <fhblf@tutanota.com>
2023-12-28 13:47:07 +0100

    changed colorscheme

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 // static const char col_cyan[]        = "#005577";
-static const char col_cyan[]        = "#000066";
+static const char col_cyan[]        = "#000080";
 //static const char col_cyan[]        = "#003399";
 static const char *colors[][3]      = {
         /*               fg         bg         border   */
