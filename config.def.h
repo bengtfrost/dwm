c9bcca3cf5ec8cc98b14f7822ea3248fc3336ef2
fhblf <fhblf@tutanota.com>
2023-12-31 00:48:38 +0100

    changed color

--- config.def.h
+++ config.def.h
@@ -21,10 +21,10 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 // static const char col_cyan[]        = "#005577";
-static const char col_cyan[]        = "#0060aa";
+static const char col_cyan[]        = "#000080";
+// static const char col_cyan[]        = "#0060aa";
 // static const char col_cyan[]        = "#0c72b7";
 // static const char col_cyan[]        = "#00004c";
-// static const char col_cyan[]        = "#000080";
 static const char *colors[][3]      = {
         /*               fg         bg         border   */
         [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
