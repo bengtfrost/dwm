040a429fea952add93a8bfef0b776d37d229b096
fhblf <fhblf@tutanota.com>
2024-01-20 10:48:44 +0100

    changed color scheme

--- config.def.h
+++ config.def.h
@@ -15,42 +15,47 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 8;        /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "monospace:pixelsize=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:pixelsize=12:antialias=true:autohint=true";
+/* default color scheme */
 // static const char col_gray1[]       = "#222222";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+// static const char col_gray2[]       = "#444444";
+// static const char col_gray3[]       = "#bbbbbb";
+// static const char col_gray4[]       = "#eeeeee";
 // static const char col_cyan[]        = "#005577";
-static const char col_cyan[]        = "#00004c";
+
+/* Dark colors alternative */
+// static const char col_gray1[]       = "#000000";
+// static const char col_cyan[]        = "#00004c";
 // static const char col_cyan[]        = "#000080";
 // static const char col_cyan[]        = "#0060aa";
 // static const char col_cyan[]        = "#0c72b7";
-static const char *colors[][3]      = {
-        /*               fg         bg         border   */
-        [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-        [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+/***************************/
 
-/* Arc dark colors customized from github.com/Zaedus/dwm-arc */
-// #define ACCENT "#3b84de"
-// #define GRAY1  "#383c4a"
-// #define GRAY1  "#2c2f3a"
-// #define GRAY2  "#404552"
-// #define GRAY3  "#4b5162"
-// #define GRAY4  "#D2D2D2"
-// #define GRAY4  "#D3DAE3"
-// #define TEXT   "#ffffff"
-// static char normfgcolor[]                = GRAY4;
-// static char normbgcolor[]                = GRAY1;
-// static char normbordercolor[]            = GRAY2;
-// static char selfgcolor[]                 = TEXT;
-// static char selbgcolor[]                 = ACCENT;
-// static char selbordercolor[]             = ACCENT;
 // static const char *colors[][3]      = {
-          /*               fg           bg           border   */
-//          [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor},
-//          [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor },
+        /*               fg         bg         border   */
+//        [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+//        [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 // };
+
+/* customized arc dark colors from github.com/Zaedus/dwm-arc */
+#define ACCENT "#3b84de"
+#define GRAY1  "#383c4a"
+// #define GRAY1  "#2c2f3a"
+#define GRAY2  "#404552"
+#define GRAY3  "#4b5162"
+#define GRAY4  "#D2D2D2"
+// #define GRAY4  "#D3DAE3"
+#define TEXT   "#ffffff"
+static char normfgcolor[]                = GRAY4;
+static char normbgcolor[]                = GRAY1;
+static char normbordercolor[]            = GRAY2;
+static char selfgcolor[]                 = TEXT;
+static char selbgcolor[]                 = ACCENT;
+static char selbordercolor[]             = ACCENT;
+static const char *colors[][3]      = {
+          /*               fg           bg           border   */
+          [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor},
+          [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor },
+};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -91,8 +96,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h", "25", NULL };
-// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, "-h", "25", NULL }; 
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h", "25", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", \
+    normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, "-h", "25", NULL }; 
 static const char *termcmd[]  = { "st", NULL };
 static const char *surf[]  = { "qutebrowser", NULL };
 
